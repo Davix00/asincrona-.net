51fec46279b5fe0ae4d6a277620be1c7d4303936
David Galaviz <galavizgdavid899@gmail.com>
2023-10-06 23:24:38 -0600

    Add functionality

---
+++ peticiones_asincronas/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace peticiones_asincronas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 353);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(465, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ejecutar Query 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(34, 186);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(465, 161);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(319, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(411, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "EJECUCIÓN DE QUERYS CON HILOS";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(566, 186);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(475, 161);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(566, 353);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(475, 28);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Ejecuta Query 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(468, 42);
+            this.label2.TabIndex = 7;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(563, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(478, 70);
+            this.label3.TabIndex = 8;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1081, 410);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ peticiones_asincronas/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//espacio de nombre para el manejo de sqlserver
+using System.Data.SqlClient;
+using System.Collections;
+
+//espacio de nombre para el manejo de hilos
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace peticiones_asincronas
+{
+    public partial class Form1 : Form
+    {
+        //consultas select per.rowguid, per.FirstName,per.LastName,per.PersonType, adr.City, adr.StateProvinceID, adr.rowguid  from Person.Person as per,
+        //Person.Address as adr;
+        //select* from Person.Person;
+
+        //varriables
+        string connectionString = "Server=DESKTOP-3SAM15G;Database=AdventureWorks2012;Integrated Security=True;"; //cadena de conexion
+        public delegate DataTable delegado(string query); //creacion del delegado
+
+
+        //funciones 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public DataTable ejecutaQuery(string query)
+        {
+            DataTable dataTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    // Ejecutar la consulta SELECT
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    adapter.Fill(dataTable);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+            return dataTable;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            button1.Enabled = false;
+
+            // Crear y ejecutar un nuevo hilo
+            Thread hilo = new Thread(() =>
+            {
+                delegado ejecuta = ejecutaQuery; // Asigna el método al delegado
+                 
+                if (button1.InvokeRequired)
+                {
+                    button1.Invoke(new MethodInvoker(delegate
+                    {
+                        dataGridView1.DataSource = ejecuta("select poh.PurchaseOrderID as OrderID, poh.VendorID, pod.PurchaseOrderDetailID as OrderDetailID, pod.ProductID, pod.UnitPrice, pod.StockedQty, sm.Name as NombreEnvio, sm.ShipBase from Purchasing.PurchaseOrderHeader as poh, Purchasing.PurchaseOrderDetail as pod, Purchasing.ShipMethod as sm where poh.VendorID = 1662 and pod.PurchaseOrderID = poh.PurchaseOrderID and sm.ShipMethodID = poh.ShipMethodID;\r\n"); // Llama al delegado con la query
+                        button1.Enabled = true;
+                    }));
+                }
+            });
+
+            hilo.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            button1.Enabled = false;
+
+            // Crear y ejecutar un nuevo hilo
+            Thread hilo2 = new Thread(() =>
+            {
+                delegado ejecuta = ejecutaQuery; // Asigna el método al delegado
+                    
+                if (button1.InvokeRequired)
+                {
+                    button1.Invoke(new MethodInvoker(delegate
+                    {
+                        dataGridView2.DataSource = ejecuta("select poh.VendorID, v.BusinessEntityID as VendorIdV, poh.PurchaseOrderID, pod.PurchaseOrderID,poh.SubTotal, pod.UnitPrice, pod.OrderQty,pod.ProductID, p.Name, p.Color, v.Name, v.AccountNumber from Purchasing.PurchaseOrderHeader as poh, Purchasing.PurchaseOrderDetail as pod, Production.Product as p, Purchasing.Vendor as v where poh.VendorID between 1568 and 1572 and pod.PurchaseOrderID = poh.PurchaseOrderID and v.BusinessEntityID = poh.VendorID and pod.ProductID = p.ProductID;\r\n"); // Llama al delegado con la query
+                        button1.Enabled = true;
+                    }));
+                }
+            });
+
+            hilo2.Start();
+        }
+    }
+}
